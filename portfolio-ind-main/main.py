717e166abbc2814de36910efdc14290c29ff7204
RafPrataham <160498230+RafPrataham@users.noreply.github.com>
2024-06-30 20:33:57 +0700

    add porto file

---
+++ portfolio-ind-main/main.py
@@ -0,0 +1,25 @@
+#Импорт
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+#Halaman Konten Berjalan
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+#Keterampilan Dinamis
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    return render_template('index.html', button_python=button_python)
+
+
+# @app.route('/form'methods=['POST'])
+# def form():
+
+if __name__ == "__main__":
+    app.run(debug=True)
